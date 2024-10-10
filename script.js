0c3893b7a34ead374281cc4e6ebb9fdfed283c04
Akshat Mishra <121314130+akshatmishra1203@users.noreply.github.com>
2024-10-10 12:03:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Form Submission (Example)
+document.querySelector('.signup-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Form submitted!'); // Add form validation or form handling here.
+});
